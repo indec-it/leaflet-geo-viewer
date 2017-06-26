6a58b54f598915e608c35fe871018546d3c0ee68
manuel <manuel@retamozo.com.ar>
2017-06-26 12:57:27 -0300

    Ejemplo San Pedro. Add Control MousePosition.

---
+++ html/js/tilesSanPedro.js
@@ -0,0 +1,14 @@
+/*
+ * @copyright 2017 @manureta @indec-it
+ * @license MIT
+ */
+/*
+L.TileLayer.SanPedro = L.TileLayer.extend({
+    getTileUrl: function(coords) {
+        return "TMS_sanpedro/{z}/{x}/{-y}.jpg" ;
+    },
+    getAttribution: function() {
+        return "<a href='http://attribution.html'>San Pedro@indec</a>"
+    }
+});
+*/
