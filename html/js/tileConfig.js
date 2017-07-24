02987fd60240a1368ee590b0e896e4921ef823ac
manureta <manuel@retamozo.com.ar>
2017-07-24 18:43:07 -0300

    Ejemplo Buenos Aires con republicacion de imagenes para afuera

--- html/js/tileConfig.js
+++ html/js/tileConfig.js
@@ -6,39 +6,47 @@ ConfigSource.data = {
 		epsg: 'EPSG:22185',
 		projString: '+proj=tmerc +lat_0=-90 +lon_0=-60 +k=1 +x_0=5500000 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
 		resolutions: [
-			320,
-            160,
-            80,
-            40,
-            20,
-            10,
-            5,
-            2.5,
-            1.25,
-            0.625
-        ],
-        origin: [5488421.75000000000000, 6234375.25000000000000],
+			       3864.47012139567,
+				1932.235060697835,
+				966.1175303489175,
+				483.05876517445876,
+				241.52938258722938,
+				120.76469129361469,
+				60.382345646807345,
+				30.191172823403672,
+				15.095586411701836,
+				7.547793205850918,
+				3.773896602925459,
+				1.8869483014627295,
+				0.9434741507313648
+],
+	origin:[5183669.384186787,5451985.903941361],
         bounds : {
         	bottom: {
-        		x: 5488421.75,
-        		y: 6234375.25
+        		x: 5183669.384186787,
+        		y: 5451985.903941361 
         	},
         	top: {
-        		x: 5554704.25,
-        		y: 6286187.75
+			x: 5810709.852556057,
+        		y: 6320419.894620156
         	}
         }
 	},
 	tms: [{
-		desc: 'San Pedro',
-		url : 'TMS_sanpedro/{z}/{x}/{y}.jpg',
+		desc: 'Buenos Aires',
+		url : 'http://geoservicios.indec.gov.ar/geoserver/gwc/service/tms/1.0.0/geocenso2010%3ABuenosAires@buenosaires@png8/{z}/{x}/{y}.png8',
 		min: 0,
 		max: 7
 	},{
-		desc: 'Buenos Aires',
-		url : 'TMS_bsas/{z}/{x}/{y}.jpg',
+		desc: 'San Pedro 2',
+			url : 'http://geoservicios.indec.gov.ar/geoserver/gwc/service/tms/1.0.0/geocenso2010%3Aspot_san_pedro2@buenosaires@png8/{z}/{x}/{y}.png8',
 		min: 0,
-		max: 8
+		max: 9
+	},{
+		desc: 'San Pedro',
+			url : 'http://geoservicios.indec.gov.ar/geoserver/gwc/service/tms/1.0.0/geocenso2010%3Aspot_san_pedro1@buenosaires@png8/{z}/{x}/{y}.png8',
+		min: 0,
+		max: 9
 	}],
 	wms: [{
 		url : 'http://geoservicios.indec.gov.ar/geoserver/ows?',
